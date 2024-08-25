1ace9a67e4c5066f063d586221519e31c36c2d22
Yaroslav Zahorodnyi <zahorodnyi.yaroslav.work@gmail.com>
2024-08-25 11:56:09 +0200

    Working vesrion 2.0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+import json
 
 import telegram
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
@@ -26,15 +27,73 @@ async def send_message(bot):
     cur_line_number = last_line_number - 0
     for i in range(last_line_number, len(file_text)):
         cur_line_number += 1
-        last_lines += file_text[i]
+        content = set_content(file_text[i])
+        if content:
+            last_lines += f"{content}\n"
 
     if last_lines:
         await bot.send_message(chat_id=CHAT_ID, text=f"{last_lines}")
     last_line_number = len(file_text)
 
+
 def read_log_file():
     with open(log_directory) as f:
         return f.readlines()
+
+
+def writing_stats(json_string):
+    with open('stats.json', 'w') as f:
+        f.write(json_string)
+
+
+def set_content(content):
+    with open('stats.json') as f:
+        string = "".join(f.readlines())
+        f.close()
+    json_string = json.loads(string)
+    all_names = [i['name'] for i in json_string]
+
+    if "joined the game" in content:
+        name = content.split("joined the game")[0].split(" ")[3].strip()
+        if name not in all_names:
+            json_string.append({"name": name, "is_online": True, "time": time.time(), "advancements": [], "kills": 0})
+        else:
+            for i in json_string:
+                if i['name'] == name:
+                    i['is_online'] = True
+                    i['time'] = time.time()
+        writing_stats(json.dumps(json_string, indent=4))
+        return f"{name} joined the game"
+
+    elif "left the game" in content:
+        name = content.split("left the game")[0].split(" ")[3].strip()
+        for i in json_string:
+            if i['name'] == name:
+                i['is_online'] = False
+                i['time'] = time.time()
+        writing_stats(json.dumps(json_string, indent=4))
+        return f"{name} left the game"
+    elif "lost connection" in content:
+        pass
+
+    elif "has made the advancement" in content:
+        name = content.split(" ")[3]
+        advancement = content.split("has made the advancement ")[1].strip()[1:-1]
+        for i in json_string:
+            if i['name'] == name:
+                i['advancements'].append(advancement)
+        writing_stats(json.dumps(json_string, indent=4))
+        return f"{name} has made the advancement: {advancement}"
+
+    elif content.split(" ")[3] in all_names:
+        name = content.split(" ")[3].strip()
+        for i in json_string:
+            if i['name'] == name:
+                i['kills'] += 1
+        writing_stats(json.dumps(json_string, indent=4))
+        return f"{name}{content.split(name)[1]}"
+
+    return ""
 
 
 async def main():
@@ -49,3 +108,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    #print(set_content("[01:30:42] [Server thread/INFO]: Olix left the game"))
